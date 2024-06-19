b506f6222ef091aac35d38116236b0b7b7e4694f
Gaurav Sharma <gauravsharma@gmail.com>
2024-06-19 23:41:14 +0530

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+function Hero() {
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>DECATHLON</h1>
+                <p>
+                    LOVE IT, THEN LET SOMEONE ELSE LOVE IT
+                </p>
+                <div className="hero-btn">
+                <button>Shop Now</button>
+                <button>Ctegory</button>
+                </div>
+            </div>
+            <div className="image">
+                <img src="/images/shoe.png" alt="image" className="hero-image"/>
+            </div>
+        </main>
+    )
+}
+
+export default Hero
